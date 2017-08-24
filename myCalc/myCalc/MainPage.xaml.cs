3cf3c0fe609a831a635ba7e1b0f463d8bd811b52
zaune <zaune@users.noreply.github.com>
2017-08-24 13:09:15 -0500

    Added Voice

--- myCalc/myCalc/MainPage.xaml.cs
+++ myCalc/myCalc/MainPage.xaml.cs
@@ -73,7 +73,7 @@ namespace myCalc
 
         }
 
-        private void calc_Click(object sender, RoutedEventArgs e)
+        private async void calc_Click(object sender, RoutedEventArgs e)
         {
             string[] currencyType = { "USD", "CAD", "EUR", "GBP", "RUB" };
             decimal[] currencyRate = { 1.00m, 1.26m, 0.85m, 0.78m, 59.03m };
@@ -109,6 +109,13 @@ namespace myCalc
             {
                 output.Text = "There was an error  ¯\\_(ツ)_/¯";
             }
+
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync(output.Text);
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+
         }
 
         private decimal sourceUS(decimal source, decimal target)
